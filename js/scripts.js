75aa3a4fc98448aa358248aec14cbca1713540b7
99zarka <mohamedmanchestrawy@gmail.com>
2025-08-11 02:01:18 +0300

    carousel for logos

--- js/scripts.js
+++ js/scripts.js
@@ -226,3 +226,39 @@
 		
 		
 })(jQuery);
+
+var swiper = new Swiper('.logos-slider', {
+    slidesPerView: 7,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    }
+});
