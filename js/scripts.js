66bf29137d0b47a17975928c32eb8ca1e0c99a34
99zarka <mohamedmanchestrawy@gmail.com>
2025-08-11 03:28:52 +0300

    sliders

--- js/scripts.js
+++ js/scripts.js
@@ -262,3 +262,20 @@ var swiper = new Swiper('.logos-slider', {
         }
     }
 });
+
+var swiper = new Swiper('.outdoors-slider', {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
