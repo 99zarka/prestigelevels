802c60a53b2c57db936eaf29678e8534d20014fa
99zarka <mohamedmanchestrawy@gmail.com>
2025-08-20 19:23:56 +0300

    lazy loading

--- js/scripts.js
+++ js/scripts.js
@@ -235,6 +235,36 @@
 		
 })(jQuery);
 
+// Lazy loading images
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyImages = document.querySelectorAll("img[data-src]");
+
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.removeAttribute("data-src");
+                    lazyImage.removeAttribute("loading");
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        // Fallback for browsers that don't support Intersection Observer
+        lazyImages.forEach(function(lazyImage) {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.removeAttribute("data-src");
+            lazyImage.removeAttribute("loading");
+        });
+    }
+});
+
 
 
 var swiper = new Swiper('.logos-slider', {
